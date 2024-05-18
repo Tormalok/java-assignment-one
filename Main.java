c6dc091c8833fe6b01440dc262b7da0b7f5080e1
Henry Amponsah <maccurang@outlook.com>
2024-05-18 07:30:37 +0000

    add \n flag to the variables to enhance readability and for aesthetics

--- Main.java
+++ Main.java
@@ -3,22 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        Days of creation
-        String dayOne = "[DAY 01] Light: God said, \"Let there be light\", and there was light. \n" +
-                "He separated the light from the darkness, calling the light day \n" +
-                "and the darkness night.\n";
+        String dayOne = """
+                [DAY 01] Light: God said, "Let there be light", and there was light.\s
+                He separated the light from the darkness, calling the light day\s
+                and the darkness night. \n
+                """;
 
-        String dayTwo = "[DAY 02] Sky: God created an expanse to separate the waters above from the waters below,\n" +
-                "calling the expanse \"sky\" or \"heaven\". \n";
+        String dayTwo = """
+                [DAY 02] Sky: God created an expanse to separate the waters above from the waters below, \s
+                calling the expanse "sky" or "heaven". \n
+                """;
 
-        String dayThree = "[DAY 03] Land and Vegetation: God gathered the waters together so that dry ground appeared, which He \n" +
-                "called \"land\" or \"earth\", and the gathered waters He called the \"seas\". He then created \n" +
-                "vegetation, including seed-bearing plants and trees.\n";
+        String dayThree = """
+                [DAY 03] Land and Vegetation: God gathered the waters together so that dry ground appeared, which He\s
+                called "land" or "earth", and the gathered waters He called the "seas". He then created\s
+                vegetation, including seed-bearing plants and trees. \n
+                """;
 
-        String dayFour = "[DAY 04] Sun, Moon, and Stars: God created the greater light (sun) to govern the day, \n" +
-                "lesser light (moon) to govern the night, and also the stars.\n";
+        String dayFour = """
+                [DAY 04] Sun, Moon, and Stars: God created the greater light (sun) to govern the day,\s
+                lesser light (moon) to govern the night, and also the stars. \n
+                """;
 
-        String dayFive = "[DAY 05] Sea Creatures and Birds: God created the creatures of the sea and every \n" +
-                "living thing that moves in the water, as well as the birds to fly above the earth.\n";
+        String dayFive = """
+                [DAY 05] Sea Creatures and Birds: God created the creatures of the sea and every\s
+                living thing that moves in the water, as well as the birds to fly above the earth. \n
+                """;
 
         String daySix = """
                 [DAY 06] Land Animals and Humans: God created the land animals, including\s
@@ -26,8 +36,10 @@ public class Main {
                 in His own image, male and female, and gave them dominion over the earth and its creatures.\n
                 """;
 
-        String daySeven = "[DAY 07] Rest: God finished His work of creation and rested on the seventh day, \n" +
-                "blessing it and making it holy.\n";
+        String daySeven = """
+                [DAY 07] Rest: God finished His work of creation and rested on the seventh day,\s
+                blessing it and making it holy. \n
+                """;
 
 
 //        Main
